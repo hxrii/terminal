300302ce7f12d72a5877c7df84b39dc97a1cc78a
Harikrishnan <harikrishnankannoth@gmail.com>
2023-05-31 16:42:10 +0530

    Making help page

--- script.js
+++ script.js
@@ -2,28 +2,66 @@ const InputField = document.getElementById('textEnter');
 
 InputField.addEventListener('input', function(event) {
     if (event.target.value == 'help') {
-      handleSubmit();
+      help();
+    }
+    else
+    {
+        deletefn();
+        const terminalHead = document.getElementById("terminal-top");
+        const text2 = ">>";
+        terminalHead.textContent = text2;
     }
   });
 
-function handleSubmit() {
+function help() {
+    const terminalHead = document.getElementById("terminal-top");
+    const text2 = "Home/help $";
+    terminalHead.textContent = text2;
     
-    const userInput = InputField.value;
-    
-    const text = "This is a typing animation effect.";
-    const typingEffect = document.getElementById("typingEffect");
 
+    
+    
+    const text = "Commands intro >> Introduction edu     >> Education Details";
+    const typingEffect = document.getElementById("typingEffect");
+    
     let charIndex = 0;
     function typeText() {
         if (charIndex < text.length) {
           typingEffect.textContent += text.charAt(charIndex);
           charIndex++;
-          setTimeout(typeText, 100);
+          setTimeout(typeText, 10);
         }
       }
+      typingEffect.innerHTML='Commands intro >> Introduction edu <br> <br>    >> Education Details'
 
    typeText();
+
+
+   
     
 
 }
+
+function deletefn(){
+        const typingEffect = document.getElementById("typingEffect");
+        const text = typingEffect.textContent;
+        const reversedText = text.split("").reverse().join("");
+        typingEffect.textContent = "";
+      
+        let charIndex = 0;
+        function removeCharacter() {
+          if (charIndex < reversedText.length) {
+            typingEffect.textContent = reversedText.slice(charIndex);
+            charIndex++;
+            setTimeout(removeCharacter, 10);
+          }
+        }
+      
+        removeCharacter();
+
+        
+      }
+      
+     
+
 
